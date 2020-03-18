a8efe0580f3a2d0314537f2426028c8549f3f8f6
GABRIEL RIBEIRO TOTINO <gabriel.totino@gmail.com>
2020-03-17 23:09:09 -0300

    Adicionando aula 1 - CAP 3

---
+++ Cap3/ByteBank/Program.cs
@@ -0,0 +1,45 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario();
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "481.913.38-82";
+            carlos.Salario = 2000;
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "123.321.654-99";
+            roberta.Salario = 5000;
+
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+            gerenciador.Registrar(carlos);
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine("Total de bonificações: R$" + gerenciador.GetTotalBonificacao());
+            
+
+
+
+            Console.Read();
+        }
+    }
+}
