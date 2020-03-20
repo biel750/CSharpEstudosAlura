cb7250e65212967cd2e88390948549986449e911
GABRIEL RIBEIRO TOTINO <gabriel.totino@gmail.com>
2020-03-19 22:59:30 -0300

    Adicionando CAP2

---
+++ Cap2/07-ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+
+
+            ContaCorrente conta = new ContaCorrente(86712540,878);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+
+
+            
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(conta.Agencia);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente(86712540, 878);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+
+
+
+            Console.Read();
+        }
+    }
+}
